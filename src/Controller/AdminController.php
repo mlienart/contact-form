25d64631dceeb7ed3e264d0071560e9f69268ed0
Magali Lienart <magalilienart@gmail.com>
2018-03-03 21:52:15 +0100

    use LoginForm

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,19 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * 
+ */
 class AdminController extends Controller {
 
+    /**
+     * 
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function contact(Request $request) {
 
-        // Check if the user is logged in
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
-
-        if ($this->getUser()) {
-            return $this->render('admin/contact.html.twig', array(
-            ));
-        }
-        // Redirect on login page if user is unlogged
-        return null;
+        // Display contact page
+        return $this->render('admin/contact.html.twig', array(
+        ));
     }
 
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,9 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Form\LoginForm;
 
+/**
+ * 
+ */
 class SecurityController extends Controller {
 
+    /**
+     * 
+     * @param Request $request
+     * @param AuthenticationUtils $authUtils
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function login(Request $request, AuthenticationUtils $authUtils) {
         // Get the login error if there is one
         $error = $authUtils->getLastAuthenticationError();
@@ -15,8 +25,14 @@ class SecurityController extends Controller {
         // Last username entered by the user
         $lastUsername = $authUtils->getLastUsername();
 
+        // Display form with prefilled username
+        $form = $this->createForm(LoginForm::class, [
+            '_username' => $lastUsername,
+        ]);
+
+        // Display login page
         return $this->render('security/login.html.twig', array(
-                    'last_username' => $lastUsername,
+                    'form' => $form->createView(),
                     'error' => $error,
         ));
     }
